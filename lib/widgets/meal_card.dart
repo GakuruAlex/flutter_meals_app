020f922a811c9ced8226f2cddfbe172d7752b739
GakuruAlex <gakurualexgicimo@gmail.com>
2026-02-14 20:42:11 +0300

    Meal Card -Show image on the card -use error builder for the image

--- lib/widgets/meal_card.dart
+++ lib/widgets/meal_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meals_app/models/meal.dart';
+import 'package:flutter_meals_app/theme/theme.dart';
 
 class MealCard extends StatelessWidget {
   const MealCard({super.key, required this.meal});
@@ -7,14 +8,45 @@ class MealCard extends StatelessWidget {
   final Meal meal;
   @override
   Widget build(BuildContext context) {
-    return Text(
-      meal.title,
-      style:
-          Theme.of(
-            context,
-          ).textTheme.bodyMedium!.copyWith(
-            color: Theme.of(context).colorScheme.onSurface,
-          ),
+    return SizedBox(
+      width: 400,
+      height: 400,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          spacing: 8,
+          children: [
+            Text(
+              meal.title,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: Theme.of(context).primaryColorLight,
+              ),
+            ),
+            Image.network(
+              meal.imageUrl,
+              errorBuilder: (context, error, stackTrace) => Expanded(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.error,
+                      color: accentColor,
+                    ),
+                    Expanded(
+                      child: Text(
+                        error.toString(),
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.error,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(child: Row()),
+          ],
+        ),
+      ),
     );
   }
 }
